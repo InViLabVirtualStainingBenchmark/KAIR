511bb2ce18dddca3bc21523e65a1889eaaf71fb2
edobor Aisosa <aisosa.edoboryemi@student.kdg.be>
2026-06-10 14:58:54 +0200

    Cleaned up unused scripts and updated some folder paths

--- train_kair_HE2IHC.sh
+++ train_kair_HE2IHC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONFIG=${1:-options/swinir/train_swinir_HE2IHC_HPC.json}
+CONFIG=${1:-options/swinir/train_swinir_HE2IHC_HPC_512.json}
 export PYTHONPATH=/code/KAIR:/usr/local/lib64/python3.9/site-packages:$PYTHONPATH
 cd /code/KAIR
 python3 main_train_psnr.py \
